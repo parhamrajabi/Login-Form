f53da5f75f4f1504d21ae5d11b5ed0e179c4dfaa
Amir Parham rajabi <parhamrajabi7981@gmail.com>
2024-08-26 23:48:17 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+
+export default function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
